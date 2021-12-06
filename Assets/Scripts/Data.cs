3b491a3b5670ac571cd98e8f4ec0660b4c15089c
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2021-12-06 10:13:29 -0500

    Created serialization system

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using Newtonsoft.Json;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Data
+{
+    // public string dataName = "Scouting Data";
+
+    public string name;
+    public string matchNumber;
+    public string teamNumber;
+
+    [JsonConstructor]
+    public Data(string name, string matchNumber, string teamNumber)
+    {
+        this.name = name;
+        this.matchNumber = matchNumber;
+        this.teamNumber = teamNumber;
+    }
+}
---
+++ Assets/Scripts/SerializeData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using System;
+using TMPro;
+
+[Serializable]
+public class SerializeData : MonoBehaviour
+{
+    public Data data;
+
+    public List<Data> allData = new List<Data>();
+
+    [SerializeField] TMP_InputField nameInputField;
+    [SerializeField] TMP_InputField matchNumberInputField;
+    [SerializeField] TMP_InputField teamNumberInputField;
+
+    public void SaveData()
+    {
+        allData.Add(data);
+    }
+
+    public void ExportData()
+    {
+        var save = JsonConvert.SerializeObject(allData, Formatting.Indented);
+
+        int randomNumber = UnityEngine.Random.Range(00000, 99999);
+
+        System.IO.File.WriteAllText(Application.dataPath + "/TestData/" + randomNumber + ".json", save);
+    }
+
+
+    public void AddNameToData()
+    {
+        data.name = nameInputField.text;
+    }
+
+    public void AddTeamNumberToData()
+    {
+        data.teamNumber = teamNumberInputField.text;
+    }
+    public void AddMatchNumberToData()
+    {
+        data.matchNumber = matchNumberInputField.text;
+    }
+}
