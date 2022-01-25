02a7912e552b8f9b926b9c626a33e756c62c0990
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-01-25 10:49:45 -0500

    Changed UI to use buttons instead of swiping

---
+++ Assets/Scripts/ButtonGroupScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonGroupScript : MonoBehaviour
+{
+    [SerializeField] Canvas beforeMatchCanvas;
+    [SerializeField] Canvas autonomousCanvas;
+    [SerializeField] Canvas teleOpCanvas;
+    [SerializeField] Canvas endGameCanvas;
+    [SerializeField] Canvas exportDataCanvas;
+    [SerializeField] Canvas settingsCanvas;
+
+    private void Start()
+    {
+        beforeMatchCanvas.enabled = true;
+        autonomousCanvas.enabled = false;
+        teleOpCanvas.enabled = false;
+        endGameCanvas.enabled = false;
+        exportDataCanvas.enabled = false;
+        settingsCanvas.enabled = false;
+    }
+
+    public void OpenBeforeMatchCanvas()
+    {
+        beforeMatchCanvas.enabled = true;
+        autonomousCanvas.enabled = false;
+        teleOpCanvas.enabled = false;
+        endGameCanvas.enabled = false;
+        exportDataCanvas.enabled = false;
+        settingsCanvas.enabled = false;
+    }
+
+    public void OpenAutonomousCanvas()
+    {
+        beforeMatchCanvas.enabled = false;
+        autonomousCanvas.enabled = true;
+        teleOpCanvas.enabled = false;
+        endGameCanvas.enabled = false;
+        exportDataCanvas.enabled = false;
+        settingsCanvas.enabled = false;
+    }
+
+    public void OpenTeleOpCanvas()
+    {
+        beforeMatchCanvas.enabled = false;
+        autonomousCanvas.enabled = false;
+        teleOpCanvas.enabled = true;
+        endGameCanvas.enabled = false;
+        exportDataCanvas.enabled = false;
+        settingsCanvas.enabled = false;
+    }
+
+    public void OpenEndGameCanvas()
+    {
+        beforeMatchCanvas.enabled = false;
+        autonomousCanvas.enabled = false;
+        teleOpCanvas.enabled = false;
+        endGameCanvas.enabled = true;
+        exportDataCanvas.enabled = false;
+        settingsCanvas.enabled = false;
+    }
+
+    public void OpenExportDataCanvas()
+    {
+        beforeMatchCanvas.enabled = false;
+        autonomousCanvas.enabled = false;
+        teleOpCanvas.enabled = false;
+        endGameCanvas.enabled = false;
+        exportDataCanvas.enabled = true;
+        settingsCanvas.enabled = false;
+    }
+
+    public void OpenSettingsCanvas()
+    {
+        beforeMatchCanvas.enabled = false;
+        autonomousCanvas.enabled = false;
+        teleOpCanvas.enabled = false;
+        endGameCanvas.enabled = false;
+        exportDataCanvas.enabled = false;
+        settingsCanvas.enabled = true;
+    }
+}
