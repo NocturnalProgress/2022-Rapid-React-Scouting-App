5f9fec14140901a27fba9ecd13c26d31a6b1dbab
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-01-31 15:16:43 -0500

    Can now scout autonomous

---
+++ Assets/Scripts/Autonomous.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autonomous : MonoBehaviour
+{
+    public Data data;
+
+    public void increaseAutonomousHigh()
+    {
+        data.autonomousHigh += 4;
+    }
+
+    public void decreaseAutonomousHigh()
+    {
+        data.autonomousHigh -= 4;
+    }
+
+    public void increaseAutonomousLow()
+    {
+        data.autonomousLow += 2;
+    }
+
+    public void decreaseAutonomousLow()
+    {
+        data.autonomousLow += 2;
+    }
+}
