55c1aaa4dd22efd586e960f717677134c4762544
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-01-31 15:16:33 -0500

    Created script for auto saving while scouting

---
+++ Assets/Scripts/AutoSave.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("DoAutoSave", 10f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void DoAutoSave()
+    {
+
+    }
+}
