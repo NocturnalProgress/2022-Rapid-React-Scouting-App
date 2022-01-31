f8da8fa51d44cc6202bfd828a527d7c2598205d9
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-01-31 15:15:47 -0500

    Scouting data is properly loaded now

---
+++ Assets/Scripts/LoadScoutingData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using TMPro;
+
+public class LoadScoutingData : MonoBehaviour
+{
+    [HideInInspector] public Data data;
+
+    [SerializeField] GameObject scrollViewContent;
+    [SerializeField] GameObject savedDataPrefab;
+
+    [SerializeField] List<Data> loadedScoutingData = new List<Data>();
+
+    [SerializeField] [HideInInspector] List<TextAsset> importedTextAssets = new List<TextAsset>();
+
+    private string path;
+
+    // Scouting data is loaded into "importedTextAssets" list as a TextAsset
+    // Finally Scouting Data is added to "loadedScoutingData" for use
+
+    private void Start()
+    {
+        path = Application.dataPath + "/Resources/TestData/";
+
+    }
+
+    public void LoadSavedData()
+    {
+        loadedScoutingData.Clear();
+
+        foreach (string scoutingDataPath in Directory.GetFiles(path, "*.json"))
+        {
+            importedTextAssets.Add(Resources.Load<TextAsset>("TestData/" + Path.GetFileNameWithoutExtension(scoutingDataPath)));
+        }
+
+        for (int i = 0; i < importedTextAssets.Count; i++)
+        {
+            var pulledDataFromFile = JsonConvert.DeserializeObject<List<Data>>(importedTextAssets[i].ToString());
+            for (int j = 0; j < pulledDataFromFile.Count; j++)
+            {
+                loadedScoutingData.Add(pulledDataFromFile[j]);
+            }
+        }
+
+        importedTextAssets.Clear();
+
+        PopulateSavedDataScrollView();
+    }
+
+    private void PopulateSavedDataScrollView()
+    {
+        for (int g = 0; g < loadedScoutingData.Count; g++)
+        {
+            GameObject instantiatedSavedData = Instantiate(savedDataPrefab, scrollViewContent.transform, false) as GameObject;
+            instantiatedSavedData.transform.Find("SelectScoutingDataButton").GetComponentInChildren<TMP_Text>().text = loadedScoutingData[g].name;
+        }
+    }
+}
