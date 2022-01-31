7609635eb803b46a510dae6a753c2a005ffaca46
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-01-31 15:14:55 -0500

    Can now scout TeleOp

---
+++ Assets/Scripts/TeleOp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleOp : MonoBehaviour
+{
+    public Data data;
+
+    public void increaseTeleOpHigh()
+    {
+        data.teleOpHigh += 2;
+    }
+
+    public void decreaseTeleOpHigh()
+    {
+        data.teleOpHigh -= 2;
+    }
+
+    public void increaseTeleOpLow()
+    {
+        data.teleOpLow += 1;
+    }
+
+    public void decreaseTeleOpLow()
+    {
+        data.teleOpLow -= 1;
+    }
+}
