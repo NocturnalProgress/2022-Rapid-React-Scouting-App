fa73b1ba3b1bfea1e26b8dc4b340ddf42c8a8adf
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-01-31 15:15:10 -0500

    Added ability to load scouting data

--- Assets/Scripts/SerializeData.cs
+++ Assets/Scripts/SerializeData.cs
@@ -4,21 +4,53 @@ using UnityEngine.UI;
 using Newtonsoft.Json;
 using System;
 using TMPro;
+using System.Data;
 
 [Serializable]
 public class SerializeData : MonoBehaviour
 {
+    [SerializeField] TextAsset jsonFile;
     public Data data;
 
     public List<Data> allData = new List<Data>();
+
+    public List<Data> autoSavedData = new List<Data>();
 
     [SerializeField] TMP_InputField nameInputField;
     [SerializeField] TMP_InputField matchNumberInputField;
     [SerializeField] TMP_InputField teamNumberInputField;
 
+    private void Start() // Autosave data starting in 5 seconds and every 10 seconds
+    {
+        //InvokeRepeating("AutoSaveData", 5f, 10f);
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (!focus)
+        {
+            autoSavedData.Clear();
+            autoSavedData.Add(data);
+            Debug.Log("Data autosaved");
+        }
+    }
+
+    //private void AutoSaveData()
+    //{
+    //    Debug.Log("Data autosaved");
+    //}
+
     public void SaveData()
     {
         allData.Add(data);
+        PrepareForNextMatch();
+    }
+
+    public void PrepareForNextMatch()
+    {
+        data.name = "";
+        data.teamNumber = "";
+        data.matchNumber += 1;
     }
 
     public void ExportData()
@@ -27,9 +59,14 @@ public class SerializeData : MonoBehaviour
 
         int randomNumber = UnityEngine.Random.Range(00000, 99999);
 
-        System.IO.File.WriteAllText(Application.dataPath + "/TestData/" + randomNumber + ".json", save);
+        System.IO.File.WriteAllText(Application.dataPath + "/Resources/TestData/" + randomNumber + ".json", save);
     }
 
+    public void LoadAllData()
+    {
+        allData = JsonConvert.DeserializeObject<List<Data>>(jsonFile.ToString());
+        Debug.Log("List" + allData);
+    }
 
     public void AddNameToData()
     {
