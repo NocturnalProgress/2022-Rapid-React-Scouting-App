9ada976a117d49397c53ad535719b03b3419f978
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-02-15 11:12:47 -0500

    Can now collect data during teleop

--- Assets/Scripts/TeleOp.cs
+++ Assets/Scripts/TeleOp.cs
@@ -1,28 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TeleOp : MonoBehaviour
 {
-    public Data data;
+    [SerializeField] SerializeData serializeData;
+
+    [SerializeField] TMP_Text teleOpHighCounter;
+    [SerializeField] TMP_Text teleOpLowCounter;
+
 
     public void increaseTeleOpHigh()
     {
-        data.teleOpHigh += 2;
+        serializeData.data.teleOpHigh += 4;
+        teleOpHighCounter.text = serializeData.data.teleOpHigh.ToString();
     }
 
     public void decreaseTeleOpHigh()
     {
-        data.teleOpHigh -= 2;
+        serializeData.data.teleOpHigh -= 4;
+        teleOpHighCounter.text = serializeData.data.teleOpHigh.ToString();
     }
 
     public void increaseTeleOpLow()
     {
-        data.teleOpLow += 1;
+        serializeData.data.teleOpLow += 2;
+        teleOpLowCounter.text = serializeData.data.teleOpLow.ToString();
     }
 
     public void decreaseTeleOpLow()
     {
-        data.teleOpLow -= 1;
+        serializeData.data.teleOpLow += 2;
+        teleOpLowCounter.text = serializeData.data.teleOpLow.ToString();
+
     }
 }
