521e58e01b491e8ad4c47dc6fe174763f4de81bd
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-02-15 11:14:39 -0500

    Can now check if team has crossed the taxi line

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -13,17 +13,19 @@ public struct Data
     public string name;
     public string matchNumber;
     public string teamNumber;
+    public string taxi;
     public int autonomousHigh;
     public int autonomousLow;
     public int teleOpHigh;
     public int teleOpLow;
 
     [JsonConstructor]
-    public Data(string name, string matchNumber, string teamNumber, int autonomousHigh, int autonomousLow, int teleOpHigh, int teleOpLow)
+    public Data(string name, string matchNumber, string teamNumber, string taxi, int autonomousHigh, int autonomousLow, int teleOpHigh, int teleOpLow)
     {
         this.name = name;
         this.matchNumber = matchNumber;
         this.teamNumber = teamNumber;
+        this.taxi = taxi;
         this.autonomousHigh = autonomousHigh;
         this.autonomousLow = autonomousLow;
         this.teleOpHigh = teleOpHigh;
