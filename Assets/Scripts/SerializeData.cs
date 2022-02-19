c45e1e9f4bd82f3ae845a926d6e24687d6a08728
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-02-19 16:58:03 -0500

    Counters can save to json

--- Assets/Scripts/SerializeData.cs
+++ Assets/Scripts/SerializeData.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Newtonsoft.Json;
 using System;
 using TMPro;
-using System.Data;
+using System.IO;
 
 [Serializable]
 public class SerializeData : MonoBehaviour
 {
     [SerializeField] TextAsset jsonFile;
     public Data data;
+
+    public Data testData;
 
     public List<Data> allData = new List<Data>();
 
@@ -20,20 +21,41 @@ public class SerializeData : MonoBehaviour
     [SerializeField] TMP_InputField matchNumberInputField;
     [SerializeField] TMP_InputField teamNumberInputField;
 
+    [SerializeField] TMP_Dropdown taxiDropdown;
+
+    [SerializeField] TMP_Text autonomousHighCounter;
+    [SerializeField] TMP_Text autonomousLowCounter;
+
+    [SerializeField] TMP_Text teleOpHighCounter;
+    [SerializeField] TMP_Text teleOpLowCounter;
+
+    [SerializeField] GameObject scrollViewContent;
+    [SerializeField] GameObject savedDataPrefab;
+
+    private string scoutingDataFolderPath;
+
     private void Start() // Autosave data starting in 5 seconds and every 10 seconds
     {
+        PrepareForNextMatch();
         //InvokeRepeating("AutoSaveData", 5f, 10f);
+        autonomousHighCounter.text = data.autonomousHigh.ToString();
+        autonomousLowCounter.text = data.autonomousLow.ToString();
+        // teleOpHighCounter.text = data.teleOpHigh.ToString();
+        // teleOpLowCounter.text = data.teleOpLow.ToString();
+
+        scoutingDataFolderPath = Application.persistentDataPath + "/ScoutingData/";
+        Debug.Log(scoutingDataFolderPath);
     }
 
-    private void OnApplicationFocus(bool focus)
-    {
-        if (!focus)
-        {
-            autoSavedData.Clear();
-            autoSavedData.Add(data);
-            Debug.Log("Data autosaved");
-        }
-    }
+    //private void OnApplicationFocus(bool focus)
+    //{
+    //    if (!focus)
+    //    {
+    //        autoSavedData.Clear();
+    //        autoSavedData.Add(data);
+    //        Debug.Log("Data autosaved");
+    //    }
+    //}
 
     //private void AutoSaveData()
     //{
@@ -51,21 +73,35 @@ public class SerializeData : MonoBehaviour
         data.name = "";
         data.teamNumber = "";
         data.matchNumber += 1;
+        data.taxi = "No";
+        data.autonomousHigh = 0;
+        data.autonomousLow = 0;
+        data.teleOpHigh = 0;
+        data.teleOpLow = 0;
+        autonomousHighCounter.text = 0.ToString();
+        autonomousLowCounter.text = 0.ToString();
+        teleOpHighCounter.text = 0.ToString();
+        teleOpLowCounter.text = 0.ToString();
     }
 
     public void ExportData()
     {
-        var save = JsonConvert.SerializeObject(allData, Formatting.Indented);
+        if (Directory.Exists(scoutingDataFolderPath))
+        {
+            //Debug.Log("Folder exists. Saving data...");
 
-        int randomNumber = UnityEngine.Random.Range(00000, 99999);
+            var save = JsonConvert.SerializeObject(allData, Formatting.Indented);
 
-        System.IO.File.WriteAllText(Application.dataPath + "/Resources/TestData/" + randomNumber + ".json", save);
-    }
+            int randomNumber = UnityEngine.Random.Range(00000, 99999);
 
-    public void LoadAllData()
-    {
-        allData = JsonConvert.DeserializeObject<List<Data>>(jsonFile.ToString());
-        Debug.Log("List" + allData);
+            File.WriteAllText(scoutingDataFolderPath + randomNumber + ".json", save);
+        }
+        else
+        {
+            //Debug.Log("Folder does not exist. Creating directory...");
+
+            Directory.CreateDirectory(scoutingDataFolderPath);
+        }
     }
 
     public void AddNameToData()
@@ -80,5 +116,10 @@ public class SerializeData : MonoBehaviour
     public void AddMatchNumberToData()
     {
         data.matchNumber = matchNumberInputField.text;
+    }
+
+    public void AddTaxiToData()
+    {
+        data.taxi = taxiDropdown.options[taxiDropdown.value].text;
     }
 }
