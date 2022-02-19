8cb868ea3c41fcaaaa10bd6d3235489eb20d42c2
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-02-19 16:56:58 -0500

    Can now load scouting data

--- Assets/Scripts/LoadScoutingData.cs
+++ Assets/Scripts/LoadScoutingData.cs
@@ -13,11 +13,19 @@ public class LoadScoutingData : MonoBehaviour
     [SerializeField] GameObject scrollViewContent;
     [SerializeField] GameObject savedDataPrefab;
 
+    private string path;
+
+    private string scoutingDataFolderPath;
+
+    private TextAsset jsonFile;
+
+    [SerializeField] GameObject loadedScoutingDataPrefab;
+
     [SerializeField] List<Data> loadedScoutingData = new List<Data>();
 
     [SerializeField] [HideInInspector] List<TextAsset> importedTextAssets = new List<TextAsset>();
 
-    private string path;
+
 
     // Scouting data is loaded into "importedTextAssets" list as a TextAsset
     // Finally Scouting Data is added to "loadedScoutingData" for use
@@ -26,29 +34,57 @@ public class LoadScoutingData : MonoBehaviour
     {
         path = Application.dataPath + "/Resources/TestData/";
 
+        scoutingDataFolderPath = Application.persistentDataPath + "/ScoutingData/";
     }
+
+    //public void LoadSavedData()
+    //{
+    //    loadedScoutingData.Clear();
+
+    //    foreach (string scoutingDataPath in Directory.GetFiles(path, "*.json"))
+    //    {
+    //        importedTextAssets.Add(Resources.Load<TextAsset>("TestData/" + Path.GetFileNameWithoutExtension(scoutingDataPath)));
+    //    }
+
+    //    for (int i = 0; i < importedTextAssets.Count; i++)
+    //    {
+    //        var pulledDataFromFile = JsonConvert.DeserializeObject<List<Data>>(importedTextAssets[i].ToString());
+    //        for (int j = 0; j < pulledDataFromFile.Count; j++)
+    //        {
+    //            loadedScoutingData.Add(pulledDataFromFile[j]);
+    //        }
+    //    }
+
+    //    importedTextAssets.Clear();
+
+    //    PopulateSavedDataScrollView();
+    //}
 
     public void LoadSavedData()
     {
-        loadedScoutingData.Clear();
-
-        foreach (string scoutingDataPath in Directory.GetFiles(path, "*.json"))
+        foreach (string newPath in Directory.GetFiles(scoutingDataFolderPath, "*.json"))
         {
-            importedTextAssets.Add(Resources.Load<TextAsset>("TestData/" + Path.GetFileNameWithoutExtension(scoutingDataPath)));
-        }
+            jsonFile = new TextAsset(File.ReadAllText(newPath));
+            //Debug.Log("jsonFile \n" + jsonFile);
 
-        for (int i = 0; i < importedTextAssets.Count; i++)
-        {
-            var pulledDataFromFile = JsonConvert.DeserializeObject<List<Data>>(importedTextAssets[i].ToString());
+            var pulledDataFromFile = JsonConvert.DeserializeObject<List<Data>>(jsonFile.ToString());
+            //Debug.Log("pulledDataFromFile: \n" + pulledDataFromFile);
+
             for (int j = 0; j < pulledDataFromFile.Count; j++)
             {
                 loadedScoutingData.Add(pulledDataFromFile[j]);
             }
+
+            //Debug.Log("loadedScoutingData: \n" + loadedScoutingData);
         }
 
-        importedTextAssets.Clear();
-
         PopulateSavedDataScrollView();
+        //foreach (Data foundScoutingData in loadedScoutingData)
+        //{
+        //    Debug.Log("foundScoutingData: \n" + foundScoutingData.matchNumber);
+        //    Debug.Log("foundScoutingData: \n" + foundScoutingData.teamNumber);
+
+        //}
     }
 
     private void PopulateSavedDataScrollView()
