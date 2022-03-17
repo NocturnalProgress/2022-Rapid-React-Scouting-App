b524276c47e70dcd6201236c90d3ba1f55c9dd0d
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-03-16 20:48:31 -0400

    Created script to view loaded scouting data

---
+++ Assets/Scripts/ViewSavedData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ViewSavedData : MonoBehaviour
+{
+    public GameObject savedDataPrefabLink;
+
+
+    public void GetSavedDataPrefab(GameObject prefabLink)
+    {
+        savedDataPrefabLink = prefabLink;
+    }
+
+    public void OpenSavedData()
+    {
+        savedDataPrefabLink.SetActive(true);
+    }
+}
