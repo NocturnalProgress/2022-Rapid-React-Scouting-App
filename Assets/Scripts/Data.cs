e135807a3f4b4c9fdc08e682edb9570065436572
NocturnalProgress <26844071+NocturnalProgress@users.noreply.github.com>
2022-03-16 20:50:33 -0400

    Added new data fields

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -8,30 +8,36 @@ using System;
 [Serializable]
 public struct Data
 {
-    // public string dataName = "Scouting Data";
-
     public string name;
-    public string matchNumber;
     public string teamNumber;
+    public int matchNumber;
     public string taxi;
     public int autonomousHigh;
     public int autonomousLow;
     public int teleOpHigh;
     public int teleOpLow;
-    public int climbLevel;
+    public string climbLevel;
+    public string alliancePartner;
+    public string drivingEffectiveness;
+    public string defenseEffectiveness;
+    public string additionalNotes;
 
 
     [JsonConstructor]
-    public Data(string name, string matchNumber, string teamNumber, string taxi, int autonomousHigh, int autonomousLow, int teleOpHigh, int teleOpLow, int climbLevel)
+    public Data(string name, string teamNumber, int matchNumber, string taxi, int autonomousHigh, int autonomousLow, int teleOpHigh, int teleOpLow, string climbLevel, string alliancePartner, string drivingEffectiveness, string defenseEffectiveness, string additionalNotes)
     {
         this.name = name;
-        this.matchNumber = matchNumber;
         this.teamNumber = teamNumber;
+        this.matchNumber = matchNumber;
         this.taxi = taxi;
         this.autonomousHigh = autonomousHigh;
         this.autonomousLow = autonomousLow;
         this.teleOpHigh = teleOpHigh;
         this.teleOpLow = teleOpLow;
         this.climbLevel = climbLevel;
+        this.alliancePartner = alliancePartner;
+        this.drivingEffectiveness = drivingEffectiveness;
+        this.defenseEffectiveness = defenseEffectiveness;
+        this.additionalNotes = additionalNotes;
     }
 }
